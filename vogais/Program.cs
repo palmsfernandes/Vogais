7b9cac62d7749efe82602addb6f3548e0a128365
Vinicius Fernandes <vinicius.fernandes@bunzlbrasil.protcapad.com.br>
2020-06-02 18:09:26 -0300

    Novo

---
+++ vogais/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vogais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vogais;
+            int espaco;
+            int vogal;
+
+            Console.Write("Digite uma frase: ");
+            vogais = Console.ReadLine().ToLower();
+
+                                   
+            char[] fraseInserida = vogais.ToCharArray();
+
+            int qntvogais = 0;
+            int qntespaco = 0;
+            for (int i = 0; i < fraseInserida.Length; i++)
+            {
+                if (fraseInserida[i].ToString() == "a")
+                    qntvogais++;
+                if (fraseInserida[i].ToString() == "e")
+                    qntvogais++;
+                if (fraseInserida[i].ToString() == "i")
+                    qntvogais++;
+                if (fraseInserida[i].ToString() == "o")
+                    qntvogais++;
+                if (fraseInserida[i].ToString() == "u")
+                    qntvogais++;
+                if (fraseInserida[i].ToString() == " ")
+                    qntespaco++;
+            }
+
+            Console.WriteLine("Quantidade vogais: " + qntvogais);
+            Console.WriteLine("Quantidade espaço: " + qntespaco);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
